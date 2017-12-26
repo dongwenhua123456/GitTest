3f85e01d7991140d208f74e65010c7e5a405a3c0
dongwenhua123456 <1072875647@qq.com>
2017-12-26 11:54:10 +0800

    创建项目

---
+++ src/cn/dongwh/test/HessianClient.java
@@ -0,0 +1,30 @@
+package cn.dongwh.test;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+
+import com.caucho.hessian.client.HessianProxyFactory;
+
+import cn.dongwh.model.User;
+import cn.dongwh.service.IFileUploadService;
+import cn.dongwh.service.IService;
+
+public class HessianClient {
+	public static void main(String[] args) throws MalformedURLException, FileNotFoundException {
+		/*String url = "http://localhost:8006/HessianServer/ServiceServlet";
+		HessianProxyFactory proxyFactory = new HessianProxyFactory();
+		IService service = (IService)proxyFactory.create(IService.class, url);
+		User user = service.getUser();
+		System.out.println(user.getName());*/
+		
+		String url = "http://localhost:8006/HessianServer/FileUploadService";
+		HessianProxyFactory proxyFactory = new HessianProxyFactory();
+		IFileUploadService uploader = (IFileUploadService)proxyFactory.create(IFileUploadService.class, url);
+		InputStream in = new BufferedInputStream(new FileInputStream("D:\\111.txt"));
+		uploader.upload("111.txt", in);
+	}
+
+}
